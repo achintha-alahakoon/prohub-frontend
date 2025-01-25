5746040b1d4310704b39464ab308719b0844ddc6
Achintha Alahakoon <149008144+achintha-alahakoon@users.noreply.github.com>
2025-01-25 10:20:21 +0530

    Updated Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,23 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import AllInterns from "./Pages/AllInterns";
 import Header from "./Components/Header";
-import UserHeader from "./Components/UserHeader";
-import Home from "./Pages/UserView/Home";
-import ContactUs from "./Pages/UserView/ContactUs";
-import AboutUs from "./Pages/UserView/AboutUs";
-import JoinWithUs from "./Pages/UserView/JoinWithUs";
-import JobDetails from "./Pages/UserView/JobDetails";
-import Apply from "./Pages/UserView/Apply";
+
 
 function App() {
-  const location = useLocation();
-
-  const pathsWithHeader = ["/", "/contact-us", "/about-us", "/join-with-us"];
-  const shouldDisplayHeader = pathsWithHeader.includes(location.pathname);
 
   return (
     <div>
-      {shouldDisplayHeader && <UserHeader />}
-      <div>
+      <Router>
+        <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/join-with-us" element={<JoinWithUs />} />
-          <Route path="/all-interns" element={<AllInterns />} />
-          <Route path="/header" element={<Header />} />
-          <Route path="/job-details" element={<JobDetails />} />
-          <Route path="/apply" element={<Apply />} />
+          <Route path="/" element={<AllInterns />} />
         </Routes>
-      </div>
+      </Router>
     </div>
   );
 }
 
-function AppWithRouter() {
-  return (
-    <Router>
-      <App />
-    </Router>
-  );
-}
-
-export default AppWithRouter;
+export default App;
