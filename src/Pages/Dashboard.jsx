6184e74b3dde961da59fef3842afb7ff8a69022a
Kavindu Janojya <kjanojya@gmail.com>
2025-02-04 01:49:26 +0530

    Add the bar chart and pie chart

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,86 +1,36 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Container } from "@mui/material";
+import Avarageoftraineers from "../Components/Avarageoftraineers";
+import "chartjs-adapter-moment";
+import "chartjs-adapter-moment/dist/chartjs-adapter-moment";
+import AvarageOfFields from "../Components/AvarageOfFields";
+
 
 const Dashboard = () => {
   return (
-    <Box
-      display="grid"
-      gridTemplateColumns="repeat(12, 1fr)"
-      gridAutoRows="140px"
-      gap="20px"
-      sx={{
-        overflowY: "auto",
-        maxHeight: "84vh",
-        "&::-webkit-scrollbar": {
-          display: "none",
-        },
-        msOverflowStyle: "none",
-        scrollbarWidth: "none",
-      }}
-    >
-      {/* ROW 1 */}
-      <Box
+    <Box>
+      <Container
+        className="grid-item"
         sx={{
-          gridColumn: "span 8",
-          gridRow: "span 2",
-          backgroundColor: "#ffffff",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          borderRadius: "15px",
+          display: "grid",
+          gridTemplateColumns: "repeat(12, 1fr)",
+          gridTemplateRows: "repeat(, 1fr)",
+          gap: 2,
         }}
       >
-      </Box>
+        <Box sx={{ gridColumn: "span 6", gridRow: "span 3" }}>
+          <Avarageoftraineers />
+        </Box>
 
-      {/* ROW 2 */}
-      <Box
-        gridColumn="span 4"
-        gridRow="span 1"
-        style={{
-          backgroundColor: "#ffffff",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          borderRadius: "15px",
-        }}
-      >
-      </Box>
-      <Box
-        gridColumn="span 4"
-        gridRow="span 1"
-        style={{
-          backgroundColor: "#ffffff",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          borderRadius: "15px",
-        }}
-      >
-      </Box>
+        <Box sx={{ gridColumn: "span 6", gridRow: "span 3" }}>
+          <AvarageOfFields /> 
+        </Box>
+        <Box sx={{ gridColomn: "span 4", gridRow: "span 3" }}>
 
-      {/* ROW 3 */}
-      <Box
-        gridColumn="span 6"
-        gridRow="span 2"
-        style={{
-          backgroundColor: "#ffffff",
-          borderRadius: "15px",
-        }}
-      >
-      </Box>
-      <Box
-        gridColumn="span 6"
-        gridRow="span 2"
-        style={{
-          backgroundColor: "#ffffff",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          borderRadius: "15px",
-        }}
-      >
-      </Box>
+        </Box>
+      </Container>
+
+
     </Box>
   );
 };
